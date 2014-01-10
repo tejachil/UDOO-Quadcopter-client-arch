64fcb3e86c5104ca06ed027d9d73f0adeca0163c
Teja Chiluvuri <tejachil@vt.edu>
2014-01-09 22:44:11 -0500

    Fixed the packet syntax to sent YPRT for each message to the server.

--- udp-control-client.js
+++ udp-control-client.js
@@ -9,6 +9,14 @@ var JOYSTICK_SENSITIVITY = 0;
 var dgram = require('dgram');
 var client = dgram.createSocket('udp4');
 
+var yaw = 0.00;
+var pitch = 0.00;
+var roll = 0.00;
+
+var throttle = ['AUTO', 0.00, 0.00];
+
+var select = 1;
+
 client.on( "message", function (message, remote) {
     console.log(remote.address + ':' + remote.port +' - ' + message);
 });
@@ -19,27 +27,42 @@ var joystick = new (require('joystick'))(0, JOYSTICK_DEADZONE, JOYSTICK_SENSITIV
 joystick.on('button', function (stream) {
 	if(stream.value=='1'){
 		var messageText = stream.number + 1;
+		//console.log(stream.number);
 		switch (stream.number){
 			case 0:
-			messageText = 'FIRE';
+				messageText = 'FIRE';
+			break;
+			case 9:
+				select = 1;
+				console.log("Switched to THROTTLE " + select);
+			break;
+			case 10:
+				select = 2;
+				console.log("Switched to THROTTLE " + select);
 			break;
 			case 11:
 			messageText = 'SELECT-UP';
+				if(select >= 2)	select = 0;
+				else	select++;
+				console.log("Switched to THROTTLE " + select);
 			break;
 			case 12:
 			messageText = 'SELECT-DOWN';
+				if(select <= 0)	select = 2;
+				else	select--;
+				console.log("Switched to THROTTLE " + select);
 			break;
 			case 13:
 			messageText = 'OPTIONS';
 			break;
 		}
 
-		messageText = 'CONTROL BTN ' + messageText;
+		/*messageText = 'CONTROL BTN ' + messageText;
 		var message = new Buffer(messageText);
 		client.send(message, 0, message.length, SERVER_PORT, SERVER_HOST, function(err, bytes) {
 			if (err) throw err;
 			console.log(messageText + ' sent to ' + SERVER_HOST +':'+ SERVER_PORT);
-		});
+		});*/
 	}
 });
 
@@ -48,26 +71,30 @@ joystick.on('axis', function (stream) {
 	switch (stream.number){
 		case 0:
 		messageText = 'X';
+		roll = 1*parseFloat((parseFloat(stream.value)*9.0/JOYSTICK_DEADZONE)).toFixed(2);
 		break;
 		case 1:
 		messageText = 'Y';
-		stream.value *= -1;
+		pitch = 1*parseFloat((parseFloat(stream.value)*9.0/JOYSTICK_DEADZONE)).toFixed(2);
 		break;
 		case 2:
 		messageText = 'THROTTLE-LEFT';
+		throttle[1] = ((1-(parseFloat(stream.value)+32767.0)/(2.0*32767.0))*100.0).toFixed(2);
 		break;
 		case 3:
 		messageText = 'Z';
+		yaw = parseFloat((parseFloat(stream.value)*9.0/JOYSTICK_DEADZONE)).toFixed(2);
 		break;
 		case 4:
 		messageText = 'THROTTLE-RIGHT';
+		throttle[2] = ((1-(parseFloat(stream.value)+32767.0)/(2.0*32767.0))*100.0).toFixed(2);
 		break;
 	}
 
-	messageText = 'CONTROL AXIS ' + messageText + ' ' + parseInt((parseInt(stream.value)*100.0/JOYSTICK_DEADZONE));
+	messageText = 'CONTROL DYNAMICS #YPRT=' + yaw + ',' + pitch + ',' + roll + ',' + throttle[select];
 	var message = new Buffer(messageText);
 	client.send(message, 0, message.length, SERVER_PORT, SERVER_HOST, function(err, bytes) {
 		if (err) throw err;
-		console.log(messageText + ' sent to ' + SERVER_HOST +':'+ SERVER_PORT);
+		console.log(messageText);// + ' sent to ' + SERVER_HOST +':'+ SERVER_PORT);
 	});
 });
